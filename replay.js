e121a727d3b5b57370c765c6bbcf8b4b0b785528
Pekka Lampila <pekka.lampila@iki.fi>
2018-03-13 12:21:33 +0000

    Match replay codes in links and before punctuation.

--- replay.js
+++ replay.js
@@ -11,7 +11,7 @@ const zlib = require('zlib');
 const config = toml.parse(fs.readFileSync('bot.toml'));
 
 const codeRegexp = /^[a-zA-Z0-9@+]{5}-[a-zA-Z0-9@+]{5}$/;
-const codeSearchRegexp = /(?:^|\s)([a-zA-Z0-9@+]{5}-[a-zA-Z0-9@+]{5})(?:\s|$)/g;
+const codeSearchRegexp = /(?:^|\s|(?:[\?\&]r=))([a-zA-Z0-9@+]{5}-[a-zA-Z0-9@+]{5})(?:[\s,\.]|\&\w+|$)/g;
 const gameTypeFormats = {
     200: 'Ranked',
     201: 'Versus',
